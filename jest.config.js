a0998db00b3768a93472bc880d96e66afe5d2eea
kawakamir <rui.kawakami@gmail.com>
2020-10-29 07:58:21 +0900

    add module mocks

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,7 @@ module.exports = {
   clearMocks: true,
   coverageDirectory: "coverage",
   preset: "ts-jest",
+  moduleNameMapper: {
+    "\\.(png|css)$": "<rootDir>/__mocks__/fileMock.js",
+  },
 };
